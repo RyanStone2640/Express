4e2db967a3f1dbf20ca34dbba20ac7eecdc48e8b
Ryan <ryan880822@gmail.com>
2023-09-07 17:27:26 +0800

    edit env file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
   };
 
   componentDidMount() {
+    console.log(process.env)
     const token = localStorage.getItem('token');
     const expiryDate = localStorage.getItem('expiryDate');
     if (!token || !expiryDate) {
